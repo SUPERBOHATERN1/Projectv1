1bdbd4ba0a5663928c036d5f1b5a808586e28c9d
JustynaBeDev <justyna0906@gmail.com>
2020-12-23 19:14:25 +0100

    scroll to section

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,23 @@
+document
+    .querySelectorAll('.top_naivgation li a[href^="#"]')
+    .forEach(trigger => {
+        trigger.onclick = function(e) {
+            e.preventDefault();
+            let hash = this.getAttribute('href');
+            let target = document.querySelector(hash);
+            let headerOffset = 100;
+            let elementPosition = target.offsetTop;
+            let offsetPosition = elementPosition - headerOffset;
 
+            window.scrollTo({
+                top: offsetPosition,
+                behavior: "smooth"
+            });
+        };
+    });
+
+
+/*
 
 const links = document.querySelectorAll(".page-header ul a");
 console.log(links)
@@ -10,10 +29,12 @@ for (const link of links) {
 function clickHandler(e) {
   e.preventDefault();
   const href = this.getAttribute("href");
+  console.log(href)
   const offsetTop = document.querySelector(href).offsetTop;
+  console.log(offsetTop)
 
   scroll({
     top: offsetTop,
     behavior: "smooth"
   });
-}
+}*/
